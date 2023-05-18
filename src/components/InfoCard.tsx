378ff2036fe337b39ef5ed9b88a2cf0c5420635a
lutzzdias <thiagolutzdias@hotmail.com>
2023-05-18 15:56:06 -0400

    docs: add linebreaks in description text

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -9,24 +9,22 @@ export function InfoCard() {
         tem a capacidade de prever a quantidade de likes e retweets que um tuíte
         pode receber com base em seu conteúdo. Esta ferramenta auxilia os
         usuários a estimarem o potencial de engajamento de suas postagens sem a
-        necessidade de publicá-las. Ao utilizar esta aplicação, você insere o
-        texto do tuíte desejado, e a inteligência artificial realiza uma análise
-        do conteúdo, identificando padrões e características que permitem fazer
-        previsões acerca do engajamento esperado, tanto em termos de likes
-        quanto de retweets. Além disso, a aplicação oferece uma funcionalidade
-        adicional de sugestão de otimização do texto. Com base em melhores
-        práticas de engajamento nas redes sociais, a inteligência artificial
-        gera uma versão aprimorada do tuíte, visando maximizar a interação do
-        público. Nosso objetivo com esta aplicação é ajudar você a melhorar sua
+        necessidade de publicá-las. <br />
+        Ao utilizar esta aplicação, você insere o texto do tuíte desejado, e a
+        inteligência artificial realiza uma análise do conteúdo, identificando
+        padrões e características que permitem fazer previsões acerca do
+        engajamento esperado, tanto em termos de likes quanto de retweets.
+        <br />
+        Além disso, a aplicação oferece uma funcionalidade adicional de sugestão
+        de otimização do texto. Com base em melhores práticas de engajamento nas
+        redes sociais, a inteligência artificial gera uma versão aprimorada do
+        tuíte, visando maximizar a interação do público. <br />
+        Nosso objetivo com esta aplicação é ajudar você a melhorar sua
         estratégia de engajamento nas redes sociais, permitindo que tome
         decisões mais embasadas e maximize o alcance de suas mensagens. Com o
         uso da inteligência artificial, é possível aprimorar a qualidade e o
         impacto de suas postagens, aumentando as chances de obter resultados
-        expressivos. Experimente esta aplicação e descubra como ela pode ser uma
-        aliada valiosa para alcançar resultados mais efetivos nas redes sociais.
-        Combinando sua criatividade com as orientações fornecidas pela
-        inteligência artificial, você pode transformar suas postagens em
-        verdadeiros sucessos de engajamento.
+        expressivos.
       </p>
     </div>
   )
